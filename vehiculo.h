dcb66d3aa60676403b6d51f12d89fad8209d2f5c
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2023-11-12 21:57:44 -0600

    Create vehiculo.h
    avance 1 de libreria de vehiculo.

---
+++ vehiculo.h
@@ -0,0 +1,53 @@
+#include <iostream>
+using namespace std;
+
+class Vehiculo{
+	
+	private:
+		string marca;
+		string tipo;
+		int ano;
+		
+	public:
+		Vehiculo(){
+		};
+		Vehiculo(string mar, string tip, int an);
+		int getAno();
+		string getMarca();
+		string getTipo();
+		void setAno(int an);
+		void setMarca(string mar);
+		void setTipo(string tip);	
+		void imprimeVehiculo();
+};
+
+int Vehiculo::getAno(){
+	return ano;
+}
+
+string Vehiculo::getMarca(){
+	return marca;
+}
+
+string Vehiculo::getTipo(){
+	return tipo;
+}
+
+void Vehiculo::setAno(int an){
+	ano = an;
+}
+
+void Vehiculo::setMarca(string mar){
+	marca = mar;
+}
+
+void Vehiculo::setTipo(string tip){
+	tipo = tip;
+}
+
+void Vehiculo::imprimeVehiculo(){
+	cout<<"Datos del vehiculo: "<<endl;
+	cout<<"Ano: "<<ano<<endl;
+	cout<<"Marca: "<<marca<<endl;
+	cout<<"Tipo: "<<tipo<<endl;
+}
