d8aa7765da3c587d7621fed4a65416aebc9f1ab2
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2023-12-02 19:12:55 -0600

    Update and rename lavado.h to Lavado.h

--- Lavado.h
+++ Lavado.h
@@ -1,25 +1,37 @@
 #include <iostream>
+
 using namespace std;
 
 class Lavado{
 	
+	//datos privados del lavado
 	private:
 		int precio;
 	
+	//constructores y setter
 	public:
 		Lavado();
+		Lavado(int pre);
 		void setPrecio(int pre);
 		void imprimeLavado() const;
 };
 
+//constructor vacio
 Lavado::Lavado() {
 	precio = 0;
 }
 
+//constructor con los datos que tendra
+Lavado::Lavado(int pre){
+	precio = pre;
+}
+
+//setter de precio
 void Lavado::setPrecio(int pre){
 	precio = pre;
 }
 
+//impresion de dato
 void Lavado::imprimeLavado() const{
-	cout<<"El precio del lavado sera de: $"<<precio<<endl;
+	cout << "El precio del lavado sera de: $" << precio << endl;
 }
