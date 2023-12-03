a78efaeda672351477fc3a2194a9fbe7da5bb414
Sangg22 <142357065+Sangg22@users.noreply.github.com>
2023-12-02 19:24:30 -0600

    Update and rename Registrocliente.h to RegistroCliente.h

---
+++ RegistroCliente.h
@@ -0,0 +1,36 @@
+#ifndef REGISTROCLIENTE_H
+#define REGISTROCLIENTE_H
+#include <iostream>
+#include <vector>
+#include "Cliente.h"
+
+using namespace std;
+
+class RegistroCliente {
+	
+	private:
+		//creacion de vector para agregar clientes
+    	vector<Cliente> clientes;
+
+	public:
+		//constructor vacio
+		RegistroCliente();
+    
+    	//funcion para agregar clientes con push_back
+		void agregarCliente(Cliente& clienteNuevo) {
+        	clientes.push_back(clienteNuevo);
+    	}
+		
+		//funcion para mostrar lista de clientes
+    	void mostrarClientes() {
+        	cout << "Lista de clientes:" << endl;
+        	for (int i = 0; i < clientes.size(); i++) {
+            	std::cout << "Nombre: " << clientes[i].getNom() << ", Numero: " << clientes[i].getNum() << std::endl;
+        	}
+    	}
+};
+
+//constructor vacio
+RegistroCliente::RegistroCliente(){	
+}
+#endif
